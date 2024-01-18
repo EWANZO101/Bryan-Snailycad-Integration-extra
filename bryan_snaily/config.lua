651c8c344e7a774817a465fda7febe05789c75c5
EWANZO101 <107235474+EWANZO101@users.noreply.github.com>
2024-01-18 12:53:39 +0000

    s

---
+++ bryan_snaily/config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.Framework = 'qbcore' -- qbcore / esx
+
+Config.Defaults = {
+    registrationStatus = 'Valid'
+}
+
+Config.API = {
+    URL = 'https://api-test.swiftpeakhosting.co.uk/v1/',
+    TOKEN = 'sng_4TiV5Zuszyn9iDRQhnOpCXvrqPbvP9-VTF4SEl2uB5zYqXm1OkHIlD3R',
+    TOKEN_HEADER_NAME = 'snaily-cad-api-token',
+}
---
+++ bryan_snaily/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'BryaN'
+
+shared_script 'config.lua'
+server_script 'server.lua'
